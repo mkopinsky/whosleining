7c282864b168279fafcfc18940c0c6adffdf132e
Shmuel Honig <shonig@gmail.com>
2018-10-17 21:03:43 -0400

    Add route for shul-info url to main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/shul-info/', methods=['GET', 'POST'])
+def shul_info():
+    # if request.method == 'POST':
+        # stuff
+    # else:
+    return render_template('shulInfo.html')
