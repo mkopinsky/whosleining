16e0d78a5b49d2660d91e1f327572064e571746b
Shmuel Honig <shonig@gmail.com>
2018-10-18 10:55:19 -0400

    Create new Shuls row upon shul info form submission

--- main.py
+++ main.py
@@ -17,7 +17,21 @@ def home():
 
 @app.route('/shul-info/', methods=['GET', 'POST'])
 def shul_info():
-    # if request.method == 'POST':
-        # stuff
-    # else:
-    return render_template('shulInfo.html')
+    if request.method == 'POST':
+        newShul = Shuls(
+            name=request.form['name'],
+            address=request.form['address'],
+            city=request.form['city'],
+            state=request.form['state'],
+            zip=request.form['zip'],
+            calendar_type=request.form['calendar-type'],
+            shabbos_signup=request.form['shabbos'],
+            yomtov_signup=request.form['yom-tov'],
+            visibility=request.form['visibility'],
+            accessibility=request.form['accessibility']
+        )
+        session.add(newShul)
+        session.commit()
+        return 'Shul added successfully'
+    else:
+        return render_template('shulInfo.html')
