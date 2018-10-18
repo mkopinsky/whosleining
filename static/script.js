3c429ec7c79c4622e1f7498d7b57ebbe515f7e22
Shmuel Honig <shonig@gmail.com>
2018-10-18 13:50:55 -0400

    Add JS to disable hidden checkbox inputs

---
+++ static/script.js
@@ -0,0 +1,8 @@
+// disable hidden inputs for checkboxes when checked so that only the checked value gets passed to server
+if ($('#shabbos').prop('checked')) {
+  $('#shabbos-hidden').prop('disabled', true);
+}
+
+if ($('#yom-tov').prop('checked')) {
+  $('#yom-tov-hidden').prop('disabled', true);
+}
