d772cab4fba9ad4717e11e0eabc37fb040e03a27
Shmuel Honig <shonig@gmail.com>
2018-10-18 10:42:23 -0400

    Add imports from sqlalchemy and models to main.py to allow for manipulation of database

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from models import Base, Users, Shuls, Weeks
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///leining.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def home():
