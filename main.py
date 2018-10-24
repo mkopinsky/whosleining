7b94dd9eddade3a878adfe92f91eee9d55522474
Shmuel Honig <shonig@gmail.com>
2018-10-23 22:24:14 -0400

    Update script tag in base.html to use url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
