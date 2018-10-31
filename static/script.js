314da55099100a39c361c388220d8769167367e8
Shmuel Honig <shonig@gmail.com>
2018-10-30 20:09:43 -0400

    Update shul info template and corresponding js to remove references to Shabbos
    At least one hebcal "event" must be "on", so Shabbos will always be set to "on"

--- static/script.js
+++ static/script.js
@@ -1,9 +1,5 @@
 // disable hidden inputs for checkboxes when checked so that only the checked value gets passed to server
 $('#submit').click(function() {
-  if ($('#shabbos').prop('checked')) {
-    $('#shabbos-hidden').prop('disabled', true);
-  }
-
   if ($('#yom-tov').prop('checked')) {
     $('#yom-tov-hidden').prop('disabled', true);
   }
