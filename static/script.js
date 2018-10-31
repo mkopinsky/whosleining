965fd5691fe8e0221157286000ba2594993af606
Shmuel Honig <shonig@gmail.com>
2018-10-30 20:46:41 -0400

    Update script.js to include code for fetching parshiot data from hebcal
    Note: the two if statements for changing values of yomtov and israel are not working

--- static/script.js
+++ static/script.js
@@ -1,3 +1,31 @@
+var $parshiot = $('#parshiot');
+var $calendar_type = $('#calendar-type');
+
+var yomtov;
+var israel;
+
+if ($('#yom-tov').prop('checked')) {
+  yomtov = 'on';
+} else {
+  yomtov = 'off';
+}
+
+if ($calendar_type.val() === 'Israel') {
+  israel = 'on';
+} else {
+  israel = 'off';
+}
+
+var hebcal = $.getJSON(
+  'https://www.hebcal.com/hebcal/?v=1&cfg=json&year=now&month=x&s=on&maj=' + yomtov + '&i=' + israel
+).done(function(data) {
+  data.items.forEach(function(item) {
+    $parshiot.append('<span>' + item.date + ' </span><span>' + item.hebrew + '</span></br>');
+  })
+}).fail(function(error) {
+  return 'The parshiot data could not be loaded';
+})
+
 // disable hidden inputs for checkboxes when checked so that only the checked value gets passed to server
 $('#submit').click(function() {
   if ($('#yom-tov').prop('checked')) {
