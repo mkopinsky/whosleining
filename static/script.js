9022f174b1a0e0987a009b8b871fb472ee20011c
Shmuel Honig <shonig@gmail.com>
2018-10-31 16:38:16 -0400

    Update script.js to ignore past dates

--- static/script.js
+++ static/script.js
@@ -19,8 +19,17 @@ if ($calendar_type.val() === 'Israel') {
 var hebcal = $.getJSON(
   'https://www.hebcal.com/hebcal/?v=1&cfg=json&year=now&month=x&s=on&maj=' + yomtov + '&i=' + israel
 ).done(function(data) {
+  var today = new Date();
+  today.setHours(0, 0, 0, 0);
   data.items.forEach(function(item) {
-    $parshiot.append('<span>' + item.date + ' </span><span>' + item.hebrew + '</span></br>');
+    // format hebcal dates in order to compare each date to today's date
+    var item_date_parts = item.date.split('-');
+    var new_date = new Date(item_date_parts[0], item_date_parts[1]-1,   item_date_parts[2]);
+    new_date.setHours(0, 0, 0, 0);
+    // if the given date is today or later, display the date
+    if (new_date >= today) {
+      $parshiot.append('<span>' + item.date + ' </span><span>' + item.hebrew + '</span></br>');
+    }
   })
 }).fail(function(error) {
   return 'The parshiot data could not be loaded';
